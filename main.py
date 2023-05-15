4a423f3a4fce29e3af17ef5b9f8fd408538fedbb
Andras Milan <43931609+ZeroZepphyre@users.noreply.github.com>
2023-05-15 12:59:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas
 
 
-# TODO 1. Create a dictionary in this format:
+# Criar um dicionário com o conteúdo do csv.
 nato_alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 dict_NATO = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# Criar uma lista que contém os fonéticos correspondentes a cada letra da palavra do usuário.
 isRunning = True
 while isRunning:
     word = input("Choose a word to translate to NATO: ").upper()
@@ -17,6 +17,6 @@ while isRunning:
         try:
             word_NATO = [dict_NATO[letter] for letter in word]
         except KeyError:
-            print("Sorry, use only letters in the alphabet.")
+            print("Sorry, use only letters in the alphabet (A-Z).")
         else:
             print(word_NATO)
